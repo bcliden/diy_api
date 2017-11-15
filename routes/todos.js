b25bd0c9433b8067c1a914d0201d502e7c9f905e
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-11-15 14:34:24 -0500

    Put route working & returning new data.

--- routes/todos.js
+++ routes/todos.js
@@ -14,12 +14,36 @@ router.get('/', function(req, res){
 
 router.post('/', function(req, res){
     db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
+        .then(function(newTodo){
+            res.status(201).json(newTodo);
+        })
+        .catch(function(err){
+            res.send(err);
+        })
+});
+
+router.get('/:todoId', function(req, res){
+    db.Todo.findById(req.params.todoId)
+        .then(function(foundTodo){
+            res.json(foundTodo);
+        })
+        .catch(function(err){
+            res.send(err);
+        })
+});
+
+router.put('/:todoId', function(req, res){
+    db.Todo.findOneAndUpdate(
+        { _id : req.params.todoId },
+        req.body,
+        { new : true }
+    )
+    .then(function(todo){
+        res.json(todo);
     })
     .catch(function(err){
         res.send(err);
     })
-});
+})
 
 module.exports = router;
