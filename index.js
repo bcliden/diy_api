539a57c595598ca25e92968f6d966b3c4631124b
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-11-15 13:49:10 -0500

    Post route working. Body parsing added.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const   express = require('express'),
         app = express(),
         todoRoutes = require('./routes/todos');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', function(req, res){
     res.send("Hello from root route")
 });
