c3d467873d8a81036ec3aa3c0b12763d8574bfb2
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-11-15 22:17:32 -0500

    Started on front-end portion. temporary jQuery added. All files & dirs working.

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const   express = require('express'),
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.send("Hello from root route")
+    res.sendFile('index.html');
 });
 
 app.use("/api/todos", todoRoutes);
