d3ee3fe6272898fac4e07aa939f5ca7eb47c7e46
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-21 12:04:43 -0500

    mongoose connect updated && error handling in place

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
-
-mongoose.set('debug', true);
-mongoose.connect(
-    'mongodb://localhost/diy-api',
-    { useMongoClient: true }
-);
+const uri = 'mongodb://localhost/diy-api';
 
 mongoose.Promise = Promise;
+mongoose.set('debug', true);
+mongoose.connect(uri, { useMongoClient: true })
+    .then(() => {
+        console.log('db connected successfully!')
+    })
+    .catch(err => {
+        console.error('db connection failed: ', err.message)
+    });
 
 module.exports.Todo = require('./todo');
